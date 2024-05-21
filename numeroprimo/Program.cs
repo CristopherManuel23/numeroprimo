c73f1bbc214e6e288bc914c48c1b0d839ce42901
brand <brand@CRISTOPHE_M_L>
2024-05-20 21:36:27 -0400

    Agregar archivos de proyecto.

---
+++ numeroprimo/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace numeroprimo;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Ingrese un número: ");
+        int numero = int.Parse (Console.ReadLine());
+
+        if (EsPrimo(numero))
+        {
+            Console.WriteLine("El número es primo.");
+        }
+        else
+        {
+            Console.WriteLine("El número no es primo.");
+        }
+    }
+
+    static bool EsPrimo(int numero)
+    {
+        if (numero <= 1) return false;
+        for (int i = 2; i < numero; i++)
+        {
+            if (numero % i == 0)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+}
